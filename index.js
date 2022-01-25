0572c230a8ad58a1df810d4ec96f27b908802a82
Ankush <ankushchhabra051@gmail.com>
2022-01-26 04:02:58 +0530

    changes made

--- index.js
+++ index.js
@@ -14,15 +14,19 @@ app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
 app.use(cors());
 app.use('/posts',postRoutes);
 app.use('/user',userRoutes);
+app.get("/",(req,res)=>{
+    res.send("Hello to memories api");
+})
 //Memories and Memories123
-// const CONNECTION_URL="mongodb+srv://Memories:Memories123@cluster0.gwhwv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+//const CONNECTION_URL="mongodb+srv://Memories:Memories123@cluster0.gwhwv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT=process.env.PORT || 5000;
 if(process.env.NODE_ENV=="production")
 {
     app.use(express.static("client/build"));
+
 }
 mongoose.connect(process.env.CONNECTION_URL ,{
 
 }).then(()=>app.listen(PORT,()=>
 console.log(`Server running on port ${PORT}`)
-)).catch((err)=>console.log(err.message));
+)).catch((err)=>console.log(err));
